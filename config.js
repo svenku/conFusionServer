9499ac007bd709d3e1675408370778c53f29f8b5
svenku <sven.kunsing@gmail.com>
2021-09-08 18:37:19 +0300

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl': 'mongodb://localhost:27017/confusion'
+  'mongoUrl': 'mongodb://localhost:27017/confusion',
+  'facebook': {
+    clientId: '252654463382089',
+    clientSecret: 'ac5a2ad41c5ee13a4b71609585832f97'
+  }
 }
